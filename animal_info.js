ad3333f4946ebba64ecdb8e6de800325c6cafc29
IOTA <shrest40@purdue.edu>
2024-11-03 05:48:11 -1200

    Add files via upload

---
+++ animal_info.js
@@ -0,0 +1,41 @@
+/** @format */
+const animals = [
+  {
+    breed: "Dalmation",
+    animal_name: "Stella",
+    age: 7,
+    feed_times: "10am 3pm 7pm",
+    food: "Pedigree Chum",
+    scoops: 2,
+    warning: "no bacon bits, no peanut butter",
+  },
+  {
+    breed: "Bulldog",
+    animal_name: "Buhmie",
+    age: 1,
+    feed_times: "9am 11am 4pm 7pm",
+    food: "Farmer's Dog",
+    scoops: 1,
+    warning: "do not enter kennel",
+  },
+  {
+    breed: "Australian Cattle Dog",
+    animal_name: "Cody",
+    age: 3,
+    feed_times: "8am 11am 3pm 7pm",
+    food: "Nutrish Active Dog",
+    scoops: 3,
+    warning: "don't touch crocodile toy",
+  },
+  {
+    breed: "Cane-Corso",
+    animal_name: "Zeus",
+    age: 1,
+    feed_times: "9am 3pm 7pm",
+    food: "Science Diet",
+    scoops: 4,
+    warning: "do not enter kennel",
+  },
+];
+
+//export default animals;
